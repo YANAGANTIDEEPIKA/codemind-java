9e0baf6515ad8004436b8ddd86edbf1279a192a1
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-09-22 14:46:46 +0530

    import java.util.Scanner; class Share {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int x=s.nextInt();int y=s.nextInt();         if(x%2==0 && (x>0 || y%2==0))         System.out.println("YES");         else         System.out.println("NO");     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Share
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int x=s.nextInt();int y=s.nextInt();
+        if(x%2==0 && (x>0 || y%2==0))
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+    }
+}     
