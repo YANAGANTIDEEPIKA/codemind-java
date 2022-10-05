2c031a96d1cad186034c805e2c4aa812fef292c3
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-10-05 14:59:52 +0530

    import java.util.Scanner; class Pattern {     public static void main(String[] args)     { 		        Scanner s=new Scanner(System.in); 		        int n=s.nextInt(); 		        int p=n; 		        for(int i=n;i>=1;i--) 		        { 		        for(int j=1;j<i;j++) 		        { 		          System.out.print(" "); 		        } 		        for(int k=1;k<=n;k++) 		        { 		            if(k==1 || k==n || i==1 ||i==n) 		            { 		                System.out.print("*"); 		            } 		            else 		            { 		                System.out.print(" "); 		            } 		        } 		        System.out.println(); 		        }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -5,16 +5,23 @@ class Pattern
     {
 		        Scanner s=new Scanner(System.in);
 		        int n=s.nextInt();
-		        int p=1;
-		        for(int i=1;i<=n;i++)
+		        int p=n;
+		        for(int i=n;i>=1;i--)
 		        {
-		        for(int j=1;j<n-1;j++)
+		        for(int j=1;j<i;j++)
 		        {
-		            System.out.print(j+"");
+		          System.out.print(" ");
 		        }
-		        for(int k=1;k<n-2;k++)
+		        for(int k=1;k<=n;k++)
 		        {
-		            System.out.print(k+"");
+		            if(k==1 || k==n || i==1 ||i==n)
+		            {
+		                System.out.print("*");
+		            }
+		            else
+		            {
+		                System.out.print(" ");
+		            }
 		        }
 		        System.out.println();
 		        }
