f2ddef45abbf5278a54cf02a519a83b729ce8529
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-10-05 14:50:56 +0530

    import java.util.Scanner; class Pattern {     public static void main(String[] args)     { 		        Scanner s=new Scanner(System.in); 		        int n=s.nextInt(); 		        int p=n; 		        for(int i=0;i<n;i++) 		        { 		        for(int j=0;j<n;j++) 		        { 		          if(i==j || i+j==n-1) 		          { 		              System.out.print(p+" "); 		          } 		          else 		          System.out.print(" "); 		        } 		        p--; 		        System.out.println(); 		        }     } }

--- Print_the_following_pattern.java
+++ Print_the_following_pattern.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
-class Sum
+class Pattern
 {
-   public static void main(String args[])
-   {
-       Scanner s=new Scanner(System.in);
-       int n=s.nextInt();
-       for(int i=1;i<=n;i++)
-       {
-           for(int j=i;j<=n;j++)
-           {
-               System.out.print(j+" ");
-           }
-           System.out.println();
-       }
-   }
+    public static void main(String[] args) 
+    {
+		        Scanner s=new Scanner(System.in);
+		        int n=s.nextInt();
+		        int p=n;
+		        for(int i=0;i<n;i++)
+		        {
+		        for(int j=0;j<n;j++)
+		        {
+		          if(i==j || i+j==n-1)
+		          {
+		              System.out.print(p+" ");
+		          }
+		          else
+		          System.out.print(" ");
+		        }
+		        p--;
+		        System.out.println();
+		        }
+    }
 }
