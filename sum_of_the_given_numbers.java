a3c779742c5b9ad5b89a66392c860a92dc524853
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-10-07 15:07:39 +0530

    import java.util.Scanner;  class Revsq  {      public static void main(String args[])      {          Scanner s=new Scanner(System.in);          int t=s.nextInt();         for(int i=0;i<t;i++)         {             int m=s.nextInt(); int n=s.nextInt();             System.out.println(m+n);         }      }  }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+ class Revsq
+ {
+     public static void main(String args[])
+     {
+         Scanner s=new Scanner(System.in);
+         int t=s.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int m=s.nextInt(); int n=s.nextInt();
+            System.out.println(m+n);
+        }
+     }
+ }
