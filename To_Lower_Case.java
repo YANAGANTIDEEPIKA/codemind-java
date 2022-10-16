6bef591aee06e6b9a07cb2a8168bccbc9e65314b
YANAGANTIDEEPIKA <96773376+YANAGANTIDEEPIKA@users.noreply.github.com>
2022-10-16 06:08:16 +0530

    import java.util.Scanner; class Rev {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         String st=s.nextLine();         System.out.println(st.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Rev
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String st=s.nextLine();
+        System.out.println(st.toLowerCase());
+    }
+}
